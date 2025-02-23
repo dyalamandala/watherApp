14e1df5f1f41bfc7b953fa4b36be85c1955f6b07
dyalamandala <divya.yalamandala05@gmail.com>
2025-02-24 00:14:28 +0530

    Add files via upload

---
+++ HelloSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftTests.swift
+//  HelloSwiftTests
+//
+//  Created by Divya on 18/02/25.
+//
+
+import Testing
+@testable import HelloSwift
+
+struct HelloSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
